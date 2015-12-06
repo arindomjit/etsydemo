71591da442c8e85d9f5439c64b8c523a09808bbc
Arindomjit Bhattacharjee <b.arindom@gmail.com>
2015-12-06 18:53:09 +0800

    associate listing with users and fix upload function

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.png"
+		validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	else
 		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.png", 
   	  	:storage => :dropbox,
@@ -12,4 +13,6 @@ class Listing < ActiveRecord::Base
 	validates :name, :description, :price, presence: true
 	validates :price, numericality: { greater_than: 0 }
 	validates :image, attachment_presence: true
+
+	belongs_to :user
 end
